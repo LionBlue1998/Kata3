31e2855d155ae7b09fd1d93785e72280d15a968d
LionBlue98 <LionBlue98@LAPTOP-1EO3F80H>
2019-10-29 17:01:41 +0000

    Kata3 final

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -1,9 +1,16 @@
 package kata3;
 
 public class Kata3 {
-
+    
     public static void main(String[] args) {
-        
+        Histogram<String> histogram = new Histogram<String>();
+        histogram.increment("gmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("ulpgc.es.com");
+        histogram.increment("gmail.com");
+        histogram.increment("ull.com");
+        HistogramDisplay histo = new HistogramDisplay(histogram);
+        histo.execute();
     }
     
 }
