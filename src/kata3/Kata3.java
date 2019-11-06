8ae086ca54b165a9e40699cc096567cf4c2588e5
LionBlue98 <LionBlue98@LAPTOP-1EO3F80H>
2019-11-06 12:06:05 +0000

    Kata3 finalizado mejor

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -4,13 +4,18 @@ public class Kata3 {
     
     public static void main(String[] args) {
         Histogram<String> histogram = new Histogram<String>();
-        histogram.increment("gmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("ulpgc.es.com");
-        histogram.increment("gmail.com");
-        histogram.increment("ull.com");
+        
+        histogram.increment("gmail.com",10);
+        
+        histogram.increment("hotmail.com",7);
+        
+        histogram.increment("ulpgc.es",6);
+        
+        histogram.increment("ull.es",5);
+        
+        histogram.increment("gmail.com",0);
+        
         HistogramDisplay histo = new HistogramDisplay(histogram);
         histo.execute();
     }
-    
 }
